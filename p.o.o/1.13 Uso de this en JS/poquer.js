d11c420f1d9385ac6a0cc1ff4f5c2723d1dadf03
nicolaslarrondo <nicolaslarrondo@liceovvh.cl>
2024-11-05 10:17:22 -0300

    ok

--- p.o.o/1.13 Uso de this en JS/poquer.js
+++
@@ -1,9 +0,0 @@
-
-function boton(elegida){
-
-    console.log("cambio a amarillo")
-    elegida.style.backgroundColor = 'yellow';
-    console.log("el texto a cambiado")
-    elegida.textContent = 'Clickeado';
-
-}
